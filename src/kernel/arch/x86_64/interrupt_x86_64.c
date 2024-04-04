37ee2ca85d0534d3cadfdf412f1233b52f1425fe
pointer-to-bios <pointer-to-bios@outlook.com>
2024-04-04 22:47:42 +0800

    在设置中断之前将所有的中断描述符定向到unsupported处理函数

--- src/kernel/arch/x86_64/interrupt_x86_64.c
+++ src/kernel/arch/x86_64/interrupt_x86_64.c
@@ -4,8 +4,14 @@
 #include <kernel/arch/x86_64/interrupt_procs.h>
 
 void interrupt_init()
-{                   
+{
     gate_descriptor_t gate;
+    trap_gate_generate(gate, interrupt_entry_sym(UNSUPPORTED));
+    for (usize i = 4; i < 256; i++)
+    {
+        interrupt_register_gate(gate, i);
+    }
+
     trap_gate_generate(gate, interrupt_entry_sym(DE));
     interrupt_register_gate(gate, 0);
     trap_gate_generate(gate, interrupt_entry_sym(UNSUPPORTED));
@@ -14,5 +20,6 @@ void interrupt_init()
     interrupt_register_gate(gate, 2);
     trap_gate_generate(gate, interrupt_entry_sym(BP));
     interrupt_register_gate(gate, 3);
+
     interrupt_open();
 }
